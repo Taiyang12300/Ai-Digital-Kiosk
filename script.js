6503cdbdbedae0e0664a06ea30e48d795ec1a72c
dlt <tawan123000@gmail.com>
2026-04-16 22:45:08 +0700

    Remove unused similarity calculation functions
    Removed calculateSimilarity and editDistance functions.

--- script.js
+++ script.js
@@ -358,27 +358,4 @@ async function initDatabase() {
     } catch (e) { setTimeout(initDatabase, 5000); }
 }
 
-function calculateSimilarity(s1, s2) {
-    let longer = s1.length < s2.length ? s2 : s1;
-    let shorter = s1.length < s2.length ? s1 : s2;
-    if (longer.length === 0) return 1.0;
-    return (longer.length - editDistance(longer, shorter)) / longer.length;
-}
-
-function editDistance(s1, s2) {
-    let costs = [];
-    for (let i = 0; i <= s1.length; i++) {
-        let lastValue = i;
-        for (let j = 0; j <= s2.length; j++) {
-            if (i === 0) costs[j] = j;
-            else if (j > 0) {
-                let newVal = costs[j - 1];
-                if (s1.charAt(i - 1) !== s2.charAt(j - 1)) newVal = Math.min(Math.min(newVal, lastValue), costs[j]) + 1;
-                costs[j - 1] = lastValue; lastValue = newVal;
-            }
-        }
-        if (i > 0) costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-}
 initDatabase();
